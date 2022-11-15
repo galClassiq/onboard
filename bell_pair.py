50e82ac5a067e36bdc4228f7f679cb85d140cd2e
galClassiq <gal@classiq.io>
2022-11-15 15:42:27 +0200

    Working bell state circuit code

--- bell_pair.py
+++ bell_pair.py
@@ -1,10 +1,19 @@
+from classiq import ModelDesigner, QUInt
+from classiq.builtin_functions.standard_gates import CXGate, HGate
+from classiq.interface.generator.model import Constraints
 
-from classiq import ModelDesigner
+constraints = Constraints(max_width=3)
 
-model_designer = ModelDesigner()
+model_designer = ModelDesigner(constraints=constraints)
+hgate_params = HGate()
+cx_params = CXGate()
 
+hgate_out = model_designer.HGate(hgate_params)
+
+cx_tgt = hgate_out["TARGET"]
+
+model_designer.CXGate(cx_params, in_wires={"TARGET": cx_tgt})
 
 circuit = model_designer.synthesize()
 
-print('me')
-
+circuit.show_interactive()
